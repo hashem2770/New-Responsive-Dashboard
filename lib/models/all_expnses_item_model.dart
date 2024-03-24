2661c63c62f8525841f9aa98ce80a476131e7c0c
hashemhassen <hashemhassen0@gmail.com>
2024-03-24 03:19:52 +0200

    create all_expnses_item_model.dart

---
+++ lib/models/all_expnses_item_model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AllExpensesItemModel {
+  final Color arrowIconColor;
+  final Color cardColor;
+  final Color iconBackgroundColor;
+  final Color moneyColor;
+  final Color labelColor;
+  final String imagePath;
+  final String label;
+
+
+  AllExpensesItemModel( {
+    required this.labelColor,
+    required this.moneyColor,
+    required this.iconBackgroundColor,
+    required this.arrowIconColor,
+    required this.cardColor,
+    required this.imagePath,
+    required this.label,
+  });
+}
