70389ae96e7f2bf47dc42ca72a934fff9d499538
hashemhassen <hashemhassen0@gmail.com>
2024-03-25 20:38:58 +0200

    extract decoration helper method

---
+++ lib/helper/methods/container_decoration.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+ShapeDecoration buildShapeDecoration() {
+  return ShapeDecoration(
+    color: Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(12),
+    ),
+  );
+}
