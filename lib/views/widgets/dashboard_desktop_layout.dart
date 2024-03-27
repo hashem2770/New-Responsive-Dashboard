f4c65465c04da84b9a807ddca31baaaa10506dae
hashemhassen <hashemhassen0@gmail.com>
2024-03-27 22:34:29 +0200

    build custom_dot.dart

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
-import 'package:responsive_dashboard/views/widgets/my_card.dart';
 import 'package:responsive_dashboard/views/widgets/my_cards_page_view.dart';
 import '../all_expenses_and_quick_invoice_section.dart';
+import 'custom_dot.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -23,7 +23,13 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 24),
         Expanded(
           flex: 2,
-          child: MyCardsPageView()
+          child: Column(
+            children: [
+              MyCardsPageView(),
+              SizedBox(height: 24),
+              CustomDotIndicator(isActive: true),
+            ],
+          )
         ),
       ],
     );
