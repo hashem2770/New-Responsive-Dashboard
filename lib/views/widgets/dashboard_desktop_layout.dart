86684b6182c6a95a2faabef89d7cc7b033ead387
hashemhassen <hashemhassen0@gmail.com>
2024-03-27 22:24:59 +0200

    build my_cards_page_view.dart

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/views/widgets/my_card.dart';
+import 'package:responsive_dashboard/views/widgets/my_cards_page_view.dart';
 import '../all_expenses_and_quick_invoice_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -22,7 +23,7 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 24),
         Expanded(
           flex: 2,
-          child: MyCard()
+          child: MyCardsPageView()
         ),
       ],
     );
