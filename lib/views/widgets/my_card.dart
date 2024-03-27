78abe82b77c57f454f6c6e58c1da2574af9de5df
hashemhassen <hashemhassen0@gmail.com>
2024-03-27 22:08:44 +0200

    build my_card.dart details

--- lib/views/widgets/my_card.dart
+++ lib/views/widgets/my_card.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dashboard/utlis/app_styles.dart';
+
+import '../../utlis/app_images.dart';
 
 class MyCard extends StatelessWidget {
   const MyCard({super.key});
@@ -17,6 +21,41 @@ class MyCard extends StatelessWidget {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title:  Text(
+                'Card Name',
+                style: AppStyles.styleRegular16(context),
+              ),
+              subtitle:  Text(
+                'Hashem Hassen',
+                style: AppStyles.styleMedium20(context),
+              ),
+              trailing: SvgPicture.asset(Assets.imagesGallery,),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '999 999 9999 99999',
+                    style: AppStyles.styleMedium20(context),
+                  ),
+                  const SizedBox(height: 4,),
+                  Text(
+                    '12/20 -142',
+                    style: AppStyles.styleRegular12(context),
+                  )
+                ],
+              ),
+
+            )
+          ],
         ),
       ),
     );
